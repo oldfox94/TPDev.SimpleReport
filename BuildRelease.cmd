41fbf8905380f74b055b9633f500e08dcf6a94e7
tpDev <raphael@tpdev.at>
2016-07-01 08:21:58 +0200

    TPDev.SimpleReport: - BuildRelease anpassungen

--- BuildRelease.cmd
+++ BuildRelease.cmd
@@ -10,6 +10,7 @@ REM copy MySQLLibrary\bin\Release\MySql.Fabric.Plugin.dll DbFactory\bin\Release
 
 echo Building Project [Service] ...
 "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" TPDev.SimpleReport.SharedLibrary\TPDev.SimpleReport.SharedLibrary.csproj /p:Configuration=Release;TargetFrameworkVersion=v4.5;TargetFrameworkProfile="";OutputPath=..\Binaries /t:Rebuild
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" TPDev.SimpleReport.Logger\TPDev.SimpleReport.Logger.csproj /p:Configuration=Release;TargetFrameworkVersion=v4.5;TargetFrameworkProfile="";OutputPath=..\Binaries /t:Rebuild
 "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" TPDev.SimpleReport.TemplateManager\TPDev.SimpleReport.TemplateManager.csproj /p:Configuration=Release;TargetFrameworkVersion=v4.5;TargetFrameworkProfile="";OutputPath=..\Binaries /t:Rebuild
 "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" TPDev.SimpleReport.Service\TPDev.SimpleReport.Service.csproj /p:Configuration=Release;TargetFrameworkVersion=v4.5;TargetFrameworkProfile="";OutputPath=..\Binaries /t:Rebuild
 
@@ -19,7 +20,7 @@ del Release\*.*  /s /q
 
 
 echo Merging Binaries ...
-"C:\Program Files (x86)\Microsoft\ILMerge\ILMerge.exe" /ndebug /copyattrs /targetplatform:4.0,"C:\Windows\Microsoft.NET\Framework64\v4.0.30319" /out:Release\TPDev.SimpleReportService.dll Binaries\TPDev.SimpleReport.SharedLibrary.dll Binaries\TPDev.SimpleReport.TemplateManager.dll Binaries\TPDev.SimpleReport.Service.dll
+"C:\Program Files (x86)\Microsoft\ILMerge\ILMerge.exe" /ndebug /copyattrs /targetplatform:4.0,"C:\Windows\Microsoft.NET\Framework64\v4.0.30319" /out:Release\TPDev.SimpleReportService.dll Binaries\TPDev.SimpleReport.SharedLibrary.dll Binaries\TPDev.SimpleReport.Logger.dll Binaries\TPDev.SimpleReport.TemplateManager.dll Binaries\TPDev.SimpleReport.Service.dll
 
 
 echo Building Project [Viewer] ...
