c97fdacebb5cb97987fc6a6f1802a7cf901d3a0d
tpDev <raphael@tpdev.at>
2016-07-05 08:12:50 +0200

    TPDev.SimpleReport: - WebControl von Chromium auf Cef geändert. - UserControl: WebBrowserOverlay hinzugefügt.

---
+++ TPDev.SimpleReport.Viewer/Views/WebBrowserOverlay.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TPDev.SimpleReport.Viewer.Views
+{
+    /// <summary>
+    /// Interaktionslogik für WebBrowserOverlay.xaml
+    /// </summary>
+    public partial class WebBrowserOverlay : UserControl
+    {
+        public WebBrowserOverlay()
+        {
+            InitializeComponent();
+        }
+    }
+}
