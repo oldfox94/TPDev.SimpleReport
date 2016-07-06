bf637c14121d17283693defba921914e9c8f1b50
tpDev <raphael@tpdev.at>
2016-07-06 13:31:00 +0200

    TPDev.SimpleReport: - Anpassungen an WebOverlay und TestApp

--- TPDev.SimpleReport.Viewer/Views/WebBrowserOverlay.xaml.cs
+++ TPDev.SimpleReport.Viewer/Views/WebBrowserOverlay.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using CefSharp.Wpf;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace TPDev.SimpleReport.Viewer.Views
 {
@@ -7,32 +9,61 @@ namespace TPDev.SimpleReport.Viewer.Views
     /// </summary>
     public partial class WebBrowserOverlay : UserControl
     {
+        private ChromiumWebBrowser m_Browser { get; set; }
         public WebBrowserOverlay()
         {
             InitializeComponent();
+            DataContext = this;
+
             Loaded += OnLoaded;
             Unloaded += OnUnloaded;
 
             Bootstrapper.Boot();
+
+            m_Browser = new ChromiumWebBrowser();
+            m_Browser.SetBinding(IsEnabledProperty, "BrowserIsEnabled");
+
+            MainGrid.Children.Add(m_Browser);
         }
 
-        private void OnLoaded(object sender, System.Windows.RoutedEventArgs e)
+        private void OnLoaded(object sender, RoutedEventArgs e)
         {
+
         }
 
-        private void OnUnloaded(object sender, System.Windows.RoutedEventArgs e)
+        private void OnUnloaded(object sender, RoutedEventArgs e)
         {
             Bootstrapper.Shutdown();
         }
 
-        public void GoTo(string url)
+        public void GoToUrl(string url)
         {
-            browserCtrl.Load(url);
+            if (m_Browser == null) return;
+            m_Browser.Load(url);
         }
 
-        //public new bool IsLoaded { get { return browserCtrl.IsLoaded; } }
-        //public bool IsLoading { get { return browserCtrl.IsLoading; } }
+        public void SetHtmlContent(string content)
+        {
+            if (m_Browser == null) return;
+        }
 
-        //public new bool IsEnabled { get { return browserCtrl.IsEnabled; } set { browserCtrl.IsEnabled = value; } }
+        public bool BrowserIsLoaded { get { return m_Browser.IsLoaded; } }
+        public bool BrowserIsLoading { get { return m_Browser.IsLoading; } }
+
+        //public bool BrowserIsEnabled { get { return m_Browser.IsEnabled; } set { m_Browser.IsEnabled = value; } }
+
+        public bool BrowserHasContent { get { return m_Browser.HasContent; } }
+
+
+        public bool BrowserIsEnabled
+        {
+            get { return (bool)GetValue(BrowserIsEnabledProperty); }
+            set { SetValue(BrowserIsEnabledProperty, value); }
+        }
+        public static DependencyProperty BrowserIsEnabledProperty =
+            DependencyProperty.Register(
+                "BrowserIsEnabled",
+                typeof(bool),
+                typeof(WebBrowserOverlay));
     }
 }
--- TestApp/MainWindow.xaml.cs
+++ TestApp/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace TestApp
 {
@@ -23,13 +10,12 @@ namespace TestApp
         public MainWindow()
         {
             InitializeComponent();
-
             Loaded += OnLoaded;
         }
 
         private void OnLoaded(object sender, RoutedEventArgs e)
         {
-            browserOverlay.GoTo("www.google.com");
+            browserOverlay.GoToUrl("www.google.com");
         }
     }
 }
