1d587eddbc10bc81365fa17cbff119500ecfc73c
tpDev <raphael@tpdev.at>
2016-07-07 17:01:12 +0200

    TPDev.SimpleReport: - BuildAll

--- CopyExternal.cmd
+++ CopyExternal.cmd
@@ -1,5 +1,10 @@
 echo Copy files from _External ...
-copy _External\*.* Release
+copy _External\*.dll Release
+copy _External\*.pak Release
+copy _External\*.exe Release
+copy _External\*.dat Release
+copy _External\locales\*.* Release\locales
+
 copy _External\*.* TPDev.SimpleReport.Viewer\bin\Debug
 copy _External\*.* TPDev.SimpleReport.Viewer\bin\Release
 copy _External\*.* TPDev.SimpleReport.Viewer\bin\x86\Debug
