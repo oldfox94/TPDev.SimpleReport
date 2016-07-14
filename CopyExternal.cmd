cb158cf7608b5761eb54a776025bb78de0eea3c7
tpDev <raphael@tpdev.at>
2016-07-14 08:53:26 +0200

    TPDev.SimpleReport: - CopyExternal.cmd anpassung

--- CopyExternal.cmd
+++ CopyExternal.cmd
@@ -3,10 +3,9 @@ copy _External\*.dll Release
 copy _External\*.pak Release
 copy _External\*.exe Release
 copy _External\*.dat Release
+copy _External\*.bin Release
 copy _External\locales\*.* Release\locales
 
-copy _External\*.* TPDev.SimpleReport.Viewer\bin\Debug
-copy _External\*.* TPDev.SimpleReport.Viewer\bin\Release
 copy _External\*.* TPDev.SimpleReport.Viewer\bin\x86\Debug
 copy _External\*.* TPDev.SimpleReport.Viewer\bin\x86\Release
 copy _External\*.* TestApp\bin\Debug
