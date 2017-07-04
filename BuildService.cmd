399146237b007f2682f6b3365411e1ef86dfcbc4
Raphael Hackel <office@tpdev.at>
2017-07-04 20:16:15 +0200

    - Unnötige Dateien gelöscht

--- BuildService.cmd
+++
@@ -1,7 +0,0 @@
-echo Building Project [Service] ...
-"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" TPDev.SimpleReport.SharedLibrary\TPDev.SimpleReport.SharedLibrary.csproj /p:Configuration=Release;OutputPath=..\_Binaries /t:Rebuild
-"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" TPDev.SimpleReport.Logger\TPDev.SimpleReport.Logger.csproj /p:Configuration=Release;OutputPath=..\_Binaries /t:Rebuild
-"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" TPDev.SimpleReport.TemplateManager\TPDev.SimpleReport.TemplateManager.csproj /p:Configuration=Release;OutputPath=..\_Binaries /t:Rebuild
-"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" TPDev.SimpleReport.Service\TPDev.SimpleReport.Service.csproj /p:Configuration=Release;OutputPath=..\_Binaries /t:Rebuild
-
-call MergeService.cmd
--- BuildViewer.cmd
+++
@@ -1,5 +0,0 @@
-echo Building Project [Viewer] ...
-"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" TPDev.SimpleReport.Viewer\TPDev.SimpleReport.Viewer.csproj /p:Configuration=Release;OutputPath=..\_Binaries /t:Rebuild
-
-echo Copy to Release
-copy _Binaries\TPDev.SimpleReport.Viewer.dll _Release\TPDev.SimpleReport.Viewer.dll
